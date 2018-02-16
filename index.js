bc18936c7b28d832d6f0bd2921d31adb238ebcd8
naokikimura <n.kimura.cap@gmail.com>
2018-02-17 01:02:35 +0900

    Refactored index.js

--- index.js
+++ index.js
@@ -25,78 +25,73 @@ const msgidGenerator = (function () {
     return { next() { return (msgid = (msgid < MAX ? msgid + 1 : 0)); } };
 }());
 
-function Client(port, host = 'localhost', timeout = 0, codecOptions = { encode: {}, decode: {} }) {
+function Client(connectOptions, codecOptions) {
     events.EventEmitter.call(this);
 
-    assert.equal(typeof port, 'number', 'Illegal argument: port');
-    assert.equal(typeof host, 'string', 'Illegal argument: host');
-    assert.equal(typeof timeout, 'number', 'Illegal argument: timeout');
-
-    const self = this;
-    const socketEvents = [ 'connect', 'end', 'timeout', 'drain', 'error', 'close' ];
     const encodeCodec = msgpack.createCodec((codecOptions || {}).encode);
     const decodeCodec = msgpack.createCodec((codecOptions || {}).decode);
 
-    function send(message, callback = function () {}) {
-        const socket = net.connect(port, host);
-        socket.setTimeout(timeout);
-        debug({ socket });
-
-        socketEvents.forEach(eventName => {
-            socket.on(eventName, (...args) => {
-                debug(`socket event [${ eventName }]`);
-                self.emit.apply(self, [eventName].concat(args));
-            });
-        });
-
-        if (message[0] === 0) {
-            socket.pipe(msgpack.createDecodeStream({ codec: decodeCodec })).on('data', message => {
-                if (debug.enabled) { debug(`received message: ${ util.inspect(message, false, null, true) }`); }
-    
-                socket.end();
-                const [ type, msgid, error, result ] = message; // Response message
-                assert.equal(type, 1);
-                assert.equal(msgid, message[1]);
-                callback.call(self, error, result, msgid);
-            });
+    Object.defineProperties(this, {
+        'encodeCodec': {
+            get() { return encodeCodec; }
+        },
+        'decodeCodec': {
+            get() { return decodeCodec; }
+        },
+        'connectOptions': {
+            get() { return connectOptions; },
+            set(value) { connectOptions = value; }
+        },
+        'close': {
+            // It is left for compatibility with v0.0.2 or earlier.
+            get() { return (() => { }); }
         }
-        const encodeStream = msgpack.createEncodeStream({ codec: encodeCodec });
+    });
+}
+
+function send(message, callback = function () {}) {
+    const self = this;
+    const socket = net.createConnection(this.connectOptions, () => {
+        const encodeStream = msgpack.createEncodeStream({ codec: this.encodeCodec });
         encodeStream.pipe(socket);
         encodeStream.write(message, (...args) => {
             if (debug.enabled) { debug(`sent message: ${ util.inspect(message, false, null, true) }`); }
             if (message[0] === 2) { callback.apply(self, args); }
         });
         encodeStream.end();
+    });
+    debug({ socket });
+
+    const socketEvents = [ 'connect', 'end', 'timeout', 'drain', 'error', 'close' ];
+    socketEvents.forEach(eventName => {
+        socket.on(eventName, (...args) => {
+            debug(`socket event [${ eventName }]`);
+            self.emit.apply(self, [eventName].concat(args));
+        });
+    });
+
+    if (message[0] === 0) {
+        socket.pipe(msgpack.createDecodeStream({ codec: this.decodeCodec })).on('data', message => {
+            if (debug.enabled) { debug(`received message: ${ util.inspect(message, false, null, true) }`); }
+
+            socket.end();
+            const [ type, msgid, error, result ] = message; // Response message
+            assert.equal(type, 1);
+            assert.equal(msgid, message[1]);
+            callback.call(self, error, result, msgid);
+        });
     }
-
-    Object.defineProperty(this, 'encodeCodec', {
-        get() { return encodeCodec; }
-    });
-
-    Object.defineProperty(this, 'decodeCodec', {
-        get() { return decodeCodec; }
-    });
-
-    Object.defineProperty(this, 'send', {
-        get() { return send; },
-        enumerable: false
-    });
-
-    // It is left for compatibility with v0.6 or earlier.
-    Object.defineProperty(this, 'close', {
-        get() { return (() => {}); }
-    });
 }
 
-function _call(type, method, ...args) {
+function call(type, method, ...args) {
     const callback = typeof args[args.length - 1] === 'function' && args.pop();
     const params = args;
     const message = [ type ].concat(type === 0 ? msgidGenerator.next() : [], method, [ params ] );
     if (callback) {
-        this.send(message, callback);
+        send.call(this, message, callback);
     } else {
         return new Promise((resolve, reject) => {
-            this.send(message, (error, ...args) => {
+            send.call(this, message, (error, ...args) => {
                 if (error) { reject(error); } else { resolve(args); }
             });
         });
@@ -104,23 +99,29 @@ function _call(type, method, ...args) {
 }
 
 function request(method, ...args) {
-    return _call.apply(this, [ 0, method ].concat(args));
+    return call.apply(this, [ 0, method ].concat(args));
 }
 
 function notify(method, ...args) {
-    return _call.apply(this, [ 2, method ].concat(args));
+    return call.apply(this, [ 2, method ].concat(args));
 }
 
 Client.prototype.request = request;
-Client.prototype.call = request; // It is left for compatibility with v0.6 or earlier.
+Client.prototype.call = request; // It is left for compatibility with v0.0.2 or earlier.
 Client.prototype.notify = notify;
 util.inherits(Client, events.EventEmitter);
 exports.Client = Client;
 
-exports.createClient = function createClient(port, host, timeout, codecOptions) {
+function createClient(port, host = 'localhost', timeout = 0, codecOptions = { encode: {}, decode: {} }) {
     debug({ port, host, timeout, codecOptions });
-    return new Client(port, host, timeout, codecOptions);
-};
+
+    assert.equal(typeof port, 'number', 'Illegal argument: port');
+    assert.equal(typeof host, 'string', 'Illegal argument: host');
+    assert.equal(typeof timeout, 'number', 'Illegal argument: timeout');
+
+    return new Client({ port, host, timeout }, codecOptions);
+}
+exports.createClient = createClient;
 
 exports.createServer = function createServer(options, codecOptions = { encode: {}, decode: {} }) {
     const encodeCodec = msgpack.createCodec((codecOptions || {}).encode);
